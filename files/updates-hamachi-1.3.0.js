8af7918a722a65974c5d9caa1654a119bf68a9ba
Giovanny Andres Gongora Granada <gioyik@mozilla-hispano.org>
2014-07-14 20:34:56 -0500

    clean hamachi files

--- files/updates-hamachi-1.3.0.js
+++
@@ -1,2 +0,0 @@
-pref("app.update.channel", "stable");
-pref("app.update.url.override", "http://update.firefoxosbuilds.org/hamachi/1.3.0/%CHANNEL%/update.xml?build_id=%BUILD_ID%&version=%VERSION%");
--- files/updates-hamachi-master.js
+++
@@ -1,2 +0,0 @@
-pref("app.update.channel", "nightly");
-pref("app.update.url.override", "http://update.firefoxosbuilds.org/hamachi/master/%CHANNEL%/update.xml?build_id=%BUILD_ID%&version=%VERSION%");
